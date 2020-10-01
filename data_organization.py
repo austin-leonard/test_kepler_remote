14c95458ed705ff0149e8fd0c6d050ca6d27fa5c
Austin Leonard <aleonard@nanohmics.com>
2020-10-01 17:08:06 -0500

    Update data_organization.py

--- data_organization.py
+++ data_organization.py
@@ -11,7 +11,7 @@ from scipy.signal import find_peaks
 keys = ["OBJECT", "OBSMODE", "QUARTER", "RADIUS", "KEPMAG"]
 hdu = 0
 
-dir_ = "../02_kepler_time_series_scripts/01_Kepler_KOI/"
+#dir_ = "../02_kepler_time_series_scripts/01_Kepler_KOI/"
 
 directories = glob.glob(dir_ + "*_Q*/")
 
@@ -22,49 +22,50 @@ flares_above_6_sigma = []
 
 # Loop through each directory and each file in it
 #for directory in directories:
-for ind,file in enumerate(glob.glob(dir_+"*.fits")):
-    # Get header contents
-    header = fits.getheader(file, hdu)
-    values.append([header.get(key) for key in keys])
-    files.append(file)   
+for dir_ in directories:
+    for ind,file in enumerate(glob.glob(dir_+"*.fits")):
+        # Get header contents
+        header = fits.getheader(file, hdu)
+        values.append([header.get(key) for key in keys])
+        files.append(file)   
     
-    # Get number of flares and flare times
-    lc_raw = fits.open(str(file))
-    raw_flux = lc_raw[1].data["PDCSAP_FLUX"]
-    time = lc_raw[1].data["TIME"]
+        # Get number of flares and flare times
+        lc_raw = fits.open(str(file))
+        raw_flux = lc_raw[1].data["PDCSAP_FLUX"]
+        time = lc_raw[1].data["TIME"]
 
-    lc = lk.LightCurve(time = time, flux = raw_flux)
-    lc = lc.remove_nans().flatten()
+        lc = lk.LightCurve(time = time, flux = raw_flux)
+        lc = lc.remove_nans().flatten()
 
-    # different cadences require different flare detection windows
-    cadence = header.get("OBSMODE")
-    if cadence == "short cadence":
-        x = lc.flux
-        median = np.median(x)
-        sigma = np.std(x)
-        flare_threshold = median + (3*sigma)
-        peaks, peak_val = find_peaks(x, height=flare_threshold, distance=30)
-        total_flares.append(len(peaks))
+        # different cadences require different flare detection windows
+        cadence = header.get("OBSMODE")
+        if cadence == "short cadence":
+            x = lc.flux
+            median = np.median(x)
+            sigma = np.std(x)
+            flare_threshold = median + (3*sigma)
+            peaks, peak_val = find_peaks(x, height=flare_threshold, distance=30)
+            total_flares.append(len(peaks))
         
-        flare_threshold_six_sigma = median + (6*sigma)
-        peaks_six, peak_val_six = find_peaks(x, height=flare_threshold_six_sigma, distance=30)
-        flares_above_6_sigma.append(len(peaks_six))
+            flare_threshold_six_sigma = median + (6*sigma)
+            peaks_six, peak_val_six = find_peaks(x, height=flare_threshold_six_sigma, distance=30)
+            flares_above_6_sigma.append(len(peaks_six))
         
-    else:
-        y = lc.flux
-        median = np.median(y)
-        sigma = np.std(y)
-        flare_threshold = median + (3*sigma)
-        peaks, peak_val = find_peaks(y, height=flare_threshold, distance=4)
-        total_flares.append(len(peaks))
+        else:
+            y = lc.flux
+            median = np.median(y)
+            sigma = np.std(y)
+            flare_threshold = median + (3*sigma)
+            peaks, peak_val = find_peaks(y, height=flare_threshold, distance=4)
+            total_flares.append(len(peaks))
         
-        flare_threshold_six_sigma = median + (6*sigma)
-        peaks_six, peak_val_six = find_peaks(y, height=flare_threshold_six_sigma, distance=4)
-        flares_above_6_sigma.append(len(peaks_six))
+            flare_threshold_six_sigma = median + (6*sigma)
+            peaks_six, peak_val_six = find_peaks(y, height=flare_threshold_six_sigma, distance=4)
+            flares_above_6_sigma.append(len(peaks_six))
     
-    print("Finished", ind, "of", len(os.listdir(dir_)))
+        print("Finished", ind, "of", len(os.listdir(dir_)))
     
-    lc_raw.close()
+        lc_raw.close()
     
 
 # Construct table
@@ -84,4 +85,4 @@ flares_sixsig = Column(name = "Flares Above Six Sigma", data = flares_above_6_si
 t.add_column(flares_sixsig)
 
 # Save table as a file
-t.write("kepler_koi.html", format = "ascii.html", overwrite = True)
+t.write("kepler_all.html", format = "ascii.html", overwrite = True)
